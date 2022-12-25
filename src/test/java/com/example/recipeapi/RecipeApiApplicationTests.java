5749b20324cd1d1e965898443025e7ae8bfc3e22
Yatin Khadilkar <ykhadilkar@vmware.com>
2022-12-25 15:44:30 -0500

    First commit

---
+++ src/test/java/com/example/recipeapi/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
