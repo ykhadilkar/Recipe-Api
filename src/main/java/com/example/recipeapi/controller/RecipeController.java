2ae95b1e74b34777a525c6df0d1b79170ad3503f
Yatin Khadilkar <ykhadilkar@vmware.com>
2023-01-22 13:28:43 -0500

    adding API docs

--- src/main/java/com/example/recipeapi/controller/RecipeController.java
+++ src/main/java/com/example/recipeapi/controller/RecipeController.java
@@ -2,33 +2,94 @@ package com.example.recipeapi.controller;
 
 import com.example.recipeapi.model.Recipe;
 import com.example.recipeapi.repository.RecipeRepository;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Recipe mgmt API",
+                version = "1.0"),
+        tags = @Tag(
+                name = "Recipes REST API"))
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/recipes")
 public class RecipeController {
     @Autowired
     private RecipeRepository recipeRepository;
 
+    @Operation(summary = "Get all recipes.", method = "GET", tags = "Recipe CRUD")
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "200",
+                description = "Recipes retrieved successfully."
+        )
+    })
+    @Transactional(readOnly = true)
     @GetMapping("")
     List<Recipe> getRecipes(){
         return (List<Recipe>) recipeRepository.findAll();
     }
 
+    @Operation(summary = "Get recipe", method = "GET", tags = "Recipe CRUD")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Recipe retrieved successfully."
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Recipe not found."
+            )
+    })
     @GetMapping("/{id}")
     Optional<Recipe> getRecipe(@PathVariable Long id){
         return recipeRepository.findById(id);
     }
 
+    @Operation(summary = "Saves provided recipe.", method = "POST", tags = "Recipe CRUD")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Recipe successfully saved.",
+                    headers = @Header(
+                            name = "Name",
+                            description = "Name",
+                            required = true,
+                            schema = @Schema(type = "string"))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Passed recipe is invalid."
+            )
+    })
     @PostMapping("")
     Recipe createRecipe(@RequestBody Recipe recipe){
         return recipeRepository.save(recipe);
     }
 
+    @Operation(summary = "Update Recipe.", method = "PATCH", tags = "Recipe CRUD")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Recipe successfully saved."
+            )
+    })
     @PutMapping("/{id}")
     Recipe updateRecipe(@RequestBody Recipe newRecipe, @PathVariable Long id){
         return recipeRepository.findById(id)
@@ -43,6 +104,13 @@ public class RecipeController {
                 });
     }
 
+    @Operation(summary = "Delete recipe.", method = "DELETE", tags = "Recipe CRUD")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Recipe successfully deleted."
+            )
+    })
     @DeleteMapping("/{id}")
     public void deleteRecipe(@PathVariable Long id){
         recipeRepository.deleteById(id);
--- src/test/java/com/example/recipeapi/RecipeApiApplicationTests.java
+++ src/test/java/com/example/recipeapi/RecipeApiApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.recipeapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecipeApiApplicationTests {
+	
+	@Autowired
+    private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+    void shouldExposeOpenAPIEndpoint() {
+
+        var responseEntity = restTemplate.getForEntity("/api-docs", String.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
